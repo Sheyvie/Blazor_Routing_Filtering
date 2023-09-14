db56e80c65311bdc2a536f26a70d3892edbe75cd
Sheyvie <sheilayvi@gmail.com>
2023-09-14 14:10:56 +0300

    First Commit

---
+++ Routing/RoutingLogic/Models/SongDto.cs
@@ -0,0 +1,17 @@
+﻿namespace RoutingLogic.Models
+{
+    public class SongDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } =string.Empty;
+        
+
+        public string Singer { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string AlbumCover { get; set; } = string.Empty;
+
+        public string DateReleased { get; set; } = string.Empty;
+
+        public int CopiesSold { get; set; } 
+    }
+}
